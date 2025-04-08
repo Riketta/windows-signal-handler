6e27ce3a1f206a3d53a748830eb4829bbb7f7556
Eugene 'Riketta' Chugaev <riketta@outlook.com>
2025-04-08 17:43:05 +0300

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use libc::{sighandler_t, signal};
+use std::sync::atomic::AtomicBool;
+
+static STOP_SIGNAL: AtomicBool = AtomicBool::new(true);
+
+fn main() {
+    let handler_ptr: fn(i32) = signal_handler;
+    let handler_addr = handler_ptr as sighandler_t;
+
+    unsafe {
+        signal(libc::SIGINT, handler_addr);
+    }
+
+    println!("Idle.");
+    while STOP_SIGNAL.load(std::sync::atomic::Ordering::Relaxed) {}
+}
+
+/// Windows will execute this function in its own thread, so this effectively turns a single-threaded application into a multi-threaded one.
+fn signal_handler(sig: i32) {
+    println!("Signal: {sig}.");
+    STOP_SIGNAL.store(false, std::sync::atomic::Ordering::Relaxed);
+}
